de2cdcf8a5ac3d8dfafbbe156d5f417740aa4009
Sauravkumar <saurav23012001@gmail.com>
2025-11-06 13:47:00 +0530

    dummy

--- src/Login.jsx
+++ src/Login.jsx
@@ -19,7 +19,7 @@ export default function Login() {
     e.preventDefault();
     setError("");
     try {
-      const res = await axios.post("http://localhost:8080/api/v1/users/login", form,{ withCredentials: true });
+      const res = await axios.post("https://stockpilot-backend-pwna.onrender.com/api/v1/users/login", form,{ withCredentials: true });
       // console.log(res.data);
       // console.log(res.data.data.accessToken);
 
